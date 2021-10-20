ce247c1a3c606a00ae5a368ea5b35d888943fe7b
Skalman <skalman@riseup.net>
2021-10-20 18:37:23 -0400

    - modified dependencies to w3f repos - removed bls12-381 temproraly till wb hash to curve is implemented   for it. - moved the has to curve auxiliary sage script to arkworks-curves

--- src/engine.rs
+++ src/engine.rs
@@ -183,7 +183,7 @@ pub trait EngineBLS {
 }
 
 /// Usual aggregate BLS signature scheme on ZCash's BLS12-381 curve.
-pub type ZBLS = UsualBLS<ark_bls12_381::Bls12_381>;
+//pub type ZBLS = UsualBLS<ark_bls12_381::Bls12_381>;
 pub type BLS377 = UsualBLS<ark_bls12_377::Bls12_377>;
 
 /// Usual aggregate BLS signature scheme on ZCash's BLS12-381 curve.
