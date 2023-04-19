0d9f98b4e1d0c9c1822b2f85cbadf145afe29b73
Skalman <skalman@riseup.net>
2023-04-19 11:17:49 -0400

    - put experimental code under experimental feature. - use hash of message point and hashed secret key for generating  witness scaler in cp.

--- src/chaum_pedersen_signature.rs
+++ src/chaum_pedersen_signature.rs
@@ -21,7 +21,7 @@ pub trait ChaumPedersenSigner<E: EngineBLS, H: DynDigest +Default + Clone> {
 
     fn generate_witness_scaler(
         &self,
-        message: &Message,
+        message_point_as_bytes: &Vec<u8>,
     ) -> <<E as EngineBLS>::PublicKeyGroup as Group>::ScalarField;
 
     fn generate_dleq_proof(
@@ -60,23 +60,24 @@ impl<E: EngineBLS, H: DynDigest +Default + Clone> ChaumPedersenSigner<E, H> for 
         message: &Message,
         bls_signature: E::SignatureGroup,
     ) -> SchnorrProof<E> {
-        let mut k =
-            <SecretKeyVT<E> as ChaumPedersenSigner<E, H>>::generate_witness_scaler(self, message);
 
         let signature_point = bls_signature;
         let message_point = message.hash_to_signature_curve::<E>();
-
-        let A_point = <<E as EngineBLS>::SignatureGroup as Group>::generator() * k;
-        let B_point = message_point * k;
-
-        let A_point_as_bytes = E::signature_point_to_byte(&A_point);
-        let B_point_as_bytes = E::signature_point_to_byte(&B_point);
 
         let signature_point_as_bytes = E::signature_point_to_byte(&signature_point);
         let message_point_as_bytes = E::signature_point_to_byte(&message_point);
         let public_key_in_signature_group_as_bytes = E::signature_point_to_byte(
             &DoublePublicKeyScheme::<E>::into_public_key_in_signature_group(self).0,
         );
+
+	let mut k =
+            <SecretKeyVT<E> as ChaumPedersenSigner<E, H>>::generate_witness_scaler(self, &message_point_as_bytes);
+
+        let A_point = <<E as EngineBLS>::SignatureGroup as Group>::generator() * k;
+        let B_point = message_point * k;
+
+        let A_point_as_bytes = E::signature_point_to_byte(&A_point);
+        let B_point_as_bytes = E::signature_point_to_byte(&B_point);
 
         let proof_basis = [
 	    message_point_as_bytes,
@@ -89,6 +90,7 @@ impl<E: EngineBLS, H: DynDigest +Default + Clone> ChaumPedersenSigner<E, H> for 
 	let hasher = <DefaultFieldHasher<H> as HashToField<<<E as EngineBLS>::PublicKeyGroup as Group>::ScalarField>>::new(&[]);
 
         let c = hasher.hash_to_field(proof_basis.as_slice(), 1)[0];
+
         let s = k - c * self.0;
 
         ::zeroize::Zeroize::zeroize(&mut k); //clear secret witness from memory
@@ -98,15 +100,18 @@ impl<E: EngineBLS, H: DynDigest +Default + Clone> ChaumPedersenSigner<E, H> for 
 
     fn generate_witness_scaler(
         &self,
-        message: &Message,
+        message_point_as_bytes: &Vec<u8>,
     ) -> <<E as EngineBLS>::PublicKeyGroup as Group>::ScalarField {
         let secret_key_as_bytes = self.to_bytes();
 
+	let mut secret_key_hasher = H::default();
+	secret_key_hasher.update(secret_key_as_bytes.as_slice());
+        let hashed_secret_key = secret_key_hasher.finalize_reset().to_vec();
+	
 	let hasher = <DefaultFieldHasher<H> as HashToField<<<E as EngineBLS>::PublicKeyGroup as Group>::ScalarField>>::new(&[]);
-
         let scalar_seed = [
-            secret_key_as_bytes,
-            message.1.clone(),
+            hashed_secret_key,
+            message_point_as_bytes.clone(),
         ].concat();
 	hasher.hash_to_field(scalar_seed.as_slice(), 1)[0]        
     }
@@ -146,6 +151,6 @@ impl<E: EngineBLS, H: DynDigest +Default + Clone> ChaumPedersenVerifier<E, H> fo
 	let hasher = <DefaultFieldHasher<H> as HashToField<<<E as EngineBLS>::PublicKeyGroup as Group>::ScalarField>>::new(&[]);
         let c_check : <<E as EngineBLS>::PublicKeyGroup as Group>::ScalarField =  hasher.hash_to_field(resulting_proof_basis.as_slice(), 1)[0];
 	
-        c_check == signature_proof.1 .0
+        c_check == signature_proof.1.0
     }
 }
